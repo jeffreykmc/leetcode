aa1efa8a1b0997f39791d1d940c16b550203577a
Jeffrey Kwan <jeffrey.kwan@gmail.com>
2023-12-16 15:40:12 +0800

    add hashmap method

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -13,6 +13,37 @@ class Solution:
                 v=v.replace(s[i],"",1)
             i=i+1
         return True
+#method 3 hashmap
+   def isAnagram(self, s: str, t: str) -> bool:
+        sdict={}
+        tdict={}
+        for i in s:
+            try:
+                sdict[i]+=1
+            except:
+                sdict[i]=1
+        for i in t:
+            try:
+                tdict[i]+=1
+            except:
+                tdict[i]=1
+        l=""
+        for i in sdict:
+            l+=i
+        for i in str(l):
+            try:
+                if sdict[i]==tdict[i]:
+                    del tdict[i]
+                    del sdict[i]
+                else:
+                    return False
+            except:
+                return False
+
+        if len(tdict)==0 and len(sdict)==0:
+            return True
+        else:
+            return False
 
 
 #string replacement method
