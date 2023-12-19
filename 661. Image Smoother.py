72c83af1698fa62049aad05d958e6359e08fd8a5
jeffkwan <jeffrey.kwan@gmail.com>
2023-12-19 23:49:53 +0800

    add 661

---
+++ 661. Image Smoother.py
@@ -0,0 +1,20 @@
+#661. Image Smoother
+#https://leetcode.com/problems/image-smoother/description/?envType=daily-question&envId=2023-12-19
+
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        #https://stackoverflow.com/questions/45765223/update-value-in-multidimensional-list-in-python
+        w=len(img[0])
+        h=len(img)
+        simg=[[0 for x in range(w)] for y in range(h)]
+        for j in range(h):
+            for i in range(w):
+                a=0
+                count=0
+                for k in range(-1,2):
+                    for l in range(-1,2):
+                        if j+k>=0 and j+k<h and i+l>=0 and i+l<w: 
+                            a+=img[j+k][i+l]   
+                            count+=1
+                simg[j][i]=floor(a/count)
+        return simg
